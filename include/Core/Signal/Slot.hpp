1d4554f93148e30dec79b84fe1ddda155a6dfc8a
konakona418 <creeperblin@outlook.com>
2025-12-02 19:37:22 +0800

    feat(core): un-sync'd slot

--- include/Core/Signal/Slot.hpp
+++ include/Core/Signal/Slot.hpp
@@ -7,15 +7,30 @@
 MOE_BEGIN_NAMESPACE
 
 template<typename Derived>
-struct Slot
-    : public MutexSynchronized<Derived>,
-      public Meta::NonCopyable<Slot<Derived>> {
+struct Slot : public Meta::NonCopyable<Slot<Derived>> {
 public:
     virtual ~Slot() = default;
 
     Slot() = default;
     Slot(Slot&&) = default;
     Slot& operator=(Slot&&) = default;
+
+    template<typename... Args>
+    void _signal(Args&&... args) {
+        static_cast<Derived*>(this)->signal(std::forward<Args>(args)...);
+    }
+};
+
+template<typename Derived>
+struct SyncSlot
+    : public MutexSynchronized<Derived>,
+      public Meta::NonCopyable<SyncSlot<Derived>> {
+public:
+    virtual ~SyncSlot() = default;
+
+    SyncSlot() = default;
+    SyncSlot(SyncSlot&&) = default;
+    SyncSlot& operator=(SyncSlot&&) = default;
 
     template<typename... Args>
     void _signal(Args&&... args) {
